443461bd442b99ed81a194e7a8c658365397a45d
JJatau <jjatau14@outlook.com>
2023-09-18 14:58:17 +0100

    added backend files to frontend branch for testing

---
+++ backend/routes/quizRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  getAllQuestions,
+  getEachQuestion
+} = require('../controllers/quiz');
+
+router.route('/').get(getAllQuestions)
+router.route('/:id').get(getEachQuestion);
+
+module.exports = router;
