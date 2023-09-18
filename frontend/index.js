ca09fec1eee918997dd691e819af68e2348424d2
ishaaq08 <ishaaq001@gmail.com>
2023-09-18 17:03:57 +0100

    Made changes to selectQuiz.html and about.html

---
+++ frontend/index.js
@@ -0,0 +1,15 @@
+
+const revealBtn = document.querySelector(".reveal-btn")
+const hiddenContent= document.querySelector(".hidden-content")
+
+
+
+function revealContent(){
+    if(hiddenContent.classList.contains("reveal-btn")){
+        hiddenContent.classList.remove("reveal-btn")
+    } else {
+        hiddenContent.classList.add("reveal-btn")
+    }
+}
+
+revealBtn.addEventListener("click", revealContent);
