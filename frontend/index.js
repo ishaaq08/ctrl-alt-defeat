4485543dc9589fe094bf007c2c59bbb06815d4bd
ishaaq08 <ishaaq001@gmail.com>
2023-09-20 10:13:24 +0100

    Design changes, implementation of carousel

--- frontend/index.js
+++ frontend/index.js
@@ -1,15 +1,40 @@
-
-const revealBtn = document.querySelector(".reveal-btn")
-const hiddenContent= document.querySelector(".hidden-content")
-
+const quizButton10 = document.querySelector(".quiz-button-10")
+const quizButton50 = document.querySelector(".quiz-button-50")
 
 
-function revealContent(){
-    if(hiddenContent.classList.contains("reveal-btn")){
-        hiddenContent.classList.remove("reveal-btn")
-    } else {
-        hiddenContent.classList.add("reveal-btn")
-    }
+// Access the button and the div tag
+const reveal = document.querySelector(".reveal-button")
+const content = document.querySelector(".content")
+
+// Adding event listener
+
+// reveal.addEventListener("click", () => {
+//     if(content.classList.contains("hidden")){
+//         content.classList.remove("hidden")
+//     } else {
+//         content.classList.add("hidden")
+//     }
+// })
+
+const reviews = [
+    "Rated 5/5 by all schools in the UK- school A",
+    "Excellent learning resource, much more captivating then traditional methods - school B",
+    "Invaluable resource - school D",
+    "With the emphasis on STEM studies, Non-STEM subjects have been neglected, however this tool greatly improves our ability to teach non-stem subjects - School E"
+]
+
+// Accessing the p tags in the div carousel 
+
+
+const text = document.getElementById("quote-display")
+
+let currentQuoteIndex = 0
+
+function displayNextQuote(){
+    text.innerHTML = reviews[currentQuoteIndex]
+    currentQuoteIndex = (currentQuoteIndex + 1) % reviews.length
 }
 
-revealBtn.addEventListener("click", revealContent);
+displayNextQuote()
+
+setInterval(displayNextQuote, 5000)
