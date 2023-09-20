bfb06301b64d36c9fb014f196764efad8959b746
JJatau <jjatau14@outlook.com>
2023-09-20 12:15:13 +0100

    popup window feature added when user successfully completes card game

---
+++ frontend/matchGame/matchGame.js
@@ -0,0 +1,76 @@
+const cards = document.querySelectorAll('.pair-container');
+
+let selectedCards = [];
+let matchedCards = [];
+function openPopup() {
+  const leftPosition = (screen.availWidth - 700) / 2;
+  const topPosition = (screen.availHeight - 700) / 2;
+  return window.open('popup.html', 'PopupWindow', `width=700,height=700,left=${leftPosition},top=${topPosition}`);
+}
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+async function getApiData() {
+  const res = await fetch(`http://localhost:3000/quiz/random/all`);
+  const data = await res.json();
+  const filteredApiData = data.slice(0, 6);
+
+  const cardsData = filteredApiData.flatMap((item) => {
+    return [item.question, item.correct_answer];
+  });
+
+  // shuffleArray(cardsData);
+  cards.forEach((card, index) => {
+    card.textContent = cardsData[index];
+  });
+  
+}
+cards.forEach((card) => {
+  card.addEventListener('click', () => {
+    if (selectedCards.length < 2 && !matchedCards.includes(cards)) {
+      selectedCards.push(card);
+
+      console.log(selectedCards)
+
+      if (selectedCards.length === 2) {
+        const [firstCard, secondCard] = selectedCards;
+        const class1 = firstCard.getAttribute('class')
+        const class2 = secondCard.getAttribute('class')
+
+        if ((class1 === class2) && (firstCard.innerHTML !== secondCard.innerHTML)) {
+          matchedCards.push(firstCard, secondCard);
+          selectedCards[0].style.backgroundColor = 'green';
+          selectedCards[1].style.backgroundColor = 'green';
+          selectedCards = [];
+
+          console.log('Correct pair chosen');
+          console.log(matchedCards)
+
+          if (matchedCards.length === cards.length) {
+            openPopup();
+          } else{
+            selectedCards = [];
+          }
+        }else {   
+          console.log('Incorrect pair chosen');
+          selectedCards[0].classList.add('changeBackground');
+          selectedCards[1].classList.add('changeBackground');
+          setTimeout(() => {
+            console.log(selectedCards);
+            selectedCards[0].classList.remove('changeBackground');
+            selectedCards[1].classList.remove('changeBackground');
+            selectedCards = [];
+          }, 1000);
+          
+        }
+      }
+    }
+  })
+})
+
+
+getApiData();
+
